bb53cf116aac1a3c6c8b375ae0ade2c0c2bd81dc
Wouter van Loon <wsvanloon@live.nl>
2018-11-05 10:40:54 +0100

    Added documentation for complete package.

---
+++ R/multiview-package.r
@@ -0,0 +1,8 @@
+#' multiview: Methods for High-Dimensional Multi-View Learning.
+#'
+#' The multiview package currently provides a function StaPLR which fits a stacked penalized logistic regression model.
+#'
+#' @author Wouter van Loon <w.s.van.loon@fsw.leidenuniv.nl>
+#' @docType package
+#' @name multiview
+NULL
