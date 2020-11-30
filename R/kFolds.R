683561086d690015c650f288f4d2d4c19ba8a9b0
wsvanloon <wsvanloon@live.nl>
2020-11-30 14:30:12 +0100

    Added licencing information

--- R/kFolds.R
+++ R/kFolds.R
@@ -1,3 +1,18 @@
+# This file is part of multiview: Methods for High-Dimensional Multi-View Learning
+# Copyright (C) 2018-2020  Wouter van Loon
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 kFolds <- function(y, k){
   out <- sample(rep(1:k, length=length(y)))
   return(out)
